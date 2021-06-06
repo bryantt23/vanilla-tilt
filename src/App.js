e935f6606d4b0ee49d921296646869877b0012c1
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-06-05 17:00:21 -0700

    Install library, is aware of on hover

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Tilt from 'react-vanilla-tilt';
+import React, { useRef, useEffect } from 'react';
 
 function App() {
+  const ref = useRef(null);
+
+  function handleMouseOverHandler() {
+    console.log('handleMouseOver');
+    console.log(
+      '🚀 ~ file: App.js ~ line 11 ~ handleMouseOverHandler ~ ref.current.src',
+      ref.current
+    );
+  }
+
+  // console.log('🚀 ~ file: App.js ~ line 8 ~ App ~ ref', ref);
+  // useEffect(() => {
+  //   const node = ref.current;
+
+  //   if (node) {
+  //     console.log('🚀 ~ file: App.js ~ line 15 ~ useEffect ~ node', node);
+  //   }
+  // }, [ref.current]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Tilt ref={ref} onMouseOver={handleMouseOverHandler}>
+        <div>You can put whatever you want inside this</div>
+      </Tilt>
     </div>
   );
 }
