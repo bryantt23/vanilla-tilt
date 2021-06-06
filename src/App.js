0a60ae1cb4d247332680a6a422f36e86f0aac5df
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-06-05 17:22:45 -0700

    add event listener, remove https://stackoverflow.com/questions/60513173/how-to-add-an-event-listener-to-useref-in-useeffect

--- src/App.js
+++ src/App.js
@@ -6,6 +6,21 @@ import React, { useRef, useEffect, useState } from 'react';
 function App() {
   const ref = useRef(null);
   const [xPos, setXPos] = useState('test');
+  const [dataTilt, setDataTilt] = useState('50');
+
+  useEffect(() => {
+    // This is ok
+    if (ref && ref.current) {
+      ref.current.addEventListener('tiltChange', function (event) {
+        console.log('🚀 ~ file: App.js ~ line 13 ~ event', event);
+      });
+    }
+
+    // Passing a function that calls your function
+    return () => {
+      ref.current.addEventListener('tiltChange');
+    };
+  }, []);
 
   function handleMouseOverHandler(e) {
     // console.log('🚀 ~ file: App.js ~ line 10 ~ handleMouseOverHandler ~ e', e);
@@ -16,6 +31,8 @@ function App() {
     //   ref.current
     // );
     setXPos(e.clientX);
+    setDataTilt(e.clientX * 10 + '');
+    console.log(e.detail);
   }
 
   // console.log('🚀 ~ file: App.js ~ line 8 ~ App ~ ref', ref);
@@ -29,9 +46,23 @@ function App() {
 
   return (
     <div className='App'>
-      <Tilt ref={ref} onMouseOver={e => handleMouseOverHandler(e)}>
+      <Tilt>
         <div>You can put whatever you want inside this</div> {xPos}
       </Tilt>
+
+      <div
+        ref={ref}
+        onMouseOver={e => handleMouseOverHandler(e)}
+        className='your-element'
+        data-tilt
+        data-tilt-max='5000'
+        data-tilt-speed='400'
+        style={{ width: 300, height: 300, margin: 10 }}
+        data-tilt-perspective={dataTilt}
+        perspective='5'
+      >
+        testing
+      </div>
     </div>
   );
 }
